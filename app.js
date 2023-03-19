f3a5acdc877d33ab347e5283dc053957c4750da1
BanPei Lim <banpei.lim@gmail.com>
2023-03-19 20:45:54 +0800

    third

--- app.js
+++ app.js
@@ -5,16 +5,29 @@ require("dotenv").config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
+// Connect to database
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to database", err);
+  });
 
-app.get("/jiaheng", (req, res) => {
-  res.send("Hello, Jia Heng!");
-});
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// Routes
+const routes = require("./routes");
+app.use("/api", routes);
+
+// Server
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
