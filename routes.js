d10f0d96019a5ec1680015a7d001877d2f097a4e
BanPei Lim <banpei.lim@gmail.com>
2024-01-10 22:34:19 +0800

    commit

--- routes.js
+++ routes.js
@@ -5,5 +5,8 @@ const controller = require("./controller");
 // Routes
 router.get("/users", controller.getAllUsers);
 router.post("/users", controller.createUser);
+router.get("/test", (req, res) => {
+  res.send("Hello, World!");
+});
 
 module.exports = router;
